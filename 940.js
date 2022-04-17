3ef00a35feb51081eb660339c39fa0275c26b7d8
linzhipeng <linzhipeng07@gmail.com>
2022-04-17 21:45:23 +0800

    Create files

---
+++ 940.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_basic_host_remote_app2=self.webpackChunk_basic_host_remote_app2||[]).push([[940],{940:(e,t,s)=>{s.r(t),s.d(t,{default:()=>n});var a=s(369),p=s.n(a);const n=()=>p().createElement("button",null,"App 2 Button")}}]);
